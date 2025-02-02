af0b57a5dfd7f5a53284680954cfd52bfbfc8a24
sauce-Git <dadad1004@naver.com>
2025-02-02 22:30:53 +0900

    feat: add sub modules

--- src/modules/auth/local.module.ts
+++ src/modules/auth/local.module.ts
@@ -1,31 +1,13 @@
 import { Module } from '@nestjs/common';
 import { LocalController } from 'src/controllers/local.controller';
-import { PrismaService } from 'src/services/prisma.service';
-import { ClientsModule, Transport } from '@nestjs/microservices';
-import { JwtModule } from '@nestjs/jwt';
 import { AuthLocalService } from 'src/services/auth/local.service';
 import { PrismaModule } from '../prisma.module';
+import { MailerModule } from '../mailer.module';
+import { TokenModule } from '../token.module';
 
 @Module({
-  imports: [
-    ClientsModule.register([
-      {
-        name: 'AUTO_MAILER',
-        transport: Transport.TCP,
-        options: {
-          host: process.env.AUTO_MAILER_HOST,
-          port: parseInt(process.env.AUTO_MAILER_PORT!),
-        },
-      },
-    ]),
-    JwtModule.register({
-      signOptions: { expiresIn: '1d' },
-      secret: process.env.JWT_SECRET,
-      global: true,
-    }),
-    PrismaModule,
-  ],
+  imports: [PrismaModule, MailerModule, TokenModule],
   controllers: [LocalController],
-  providers: [AuthLocalService, PrismaService],
+  providers: [AuthLocalService],
 })
 export class AuthLocalModule {}
