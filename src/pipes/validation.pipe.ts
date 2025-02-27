a3d9b848d6743324686688214834ae58dc652af0
sauce-Git <dadad1004@naver.com>
2025-02-28 04:12:57 +0900

    chore: update validator logic

--- src/pipes/validation.pipe.ts
+++ src/pipes/validation.pipe.ts
@@ -1,15 +1,10 @@
-import { BadRequestException, Injectable, PipeTransform } from '@nestjs/common';
-import { z, ZodError, ZodSchema } from 'zod';
+import { Injectable, PipeTransform } from '@nestjs/common';
+import { z, ZodSchema } from 'zod';
 
 @Injectable()
 export class ZodValidationPipe implements PipeTransform {
   constructor(private validator: ZodSchema) {}
   async transform(value: any): Promise<z.infer<typeof this.validator>> {
-    const result = await this.validator
-      .parseAsync(value)
-      .catch((error: ZodError) => {
-        throw new BadRequestException(error.errors);
-      });
-    return result;
+    return await this.validator.parseAsync(value);
   }
 }
